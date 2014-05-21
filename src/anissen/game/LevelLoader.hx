e5505bc1f8fa539719053a1a3ebd6b293a103bb6
Anders Nissen <andnis@gmail.com>
2014-05-21 22:56:59 +0200

    Add object handling to level loading

--- src/anissen/game/LevelLoader.hx
+++ src/anissen/game/LevelLoader.hx
@@ -18,10 +18,8 @@ class LevelLoader
             return tilemap;
         }
 
-        var layers = tiledData.layers;
+        var layers :Dynamic = tiledData.layers;
         var tilesets = tiledData.tilesets;
-
-        var layer :Dynamic = layers[0]; // TODO: HACK
         var tileset :Dynamic = tilesets[0]; // TODO: HACK
 
         tilemap.init(tiledData.width, tiledData.height);
@@ -29,52 +27,42 @@ class LevelLoader
         var tilesetImage :String = tileset.image;
         var layerTexture = ctx.pack.getTexture("tilesets/" + tilesetImage.split('.png')[0]);
         
-        for (index in 0...layer.data.length) {
-            var x = layer.x + index % layer.width;
-            var y = Math.floor(index / layer.width);
-            // var entity = new Entity();
-            // tilemap.setTile(entity, x, y);
-            var entity = tilemap.getTile(x, y);
+        for (layerIndex in 0...layers.length) {
+            var layer :Dynamic = layers[layerIndex];
+            if (layer.type == "tilelayer") {
+                for (index in 0...layer.data.length) {
+                    var x = layer.x + index % layer.width;
+                    var y = layer.y + Math.floor(index / layer.width);
+                    var entity = tilemap.getTile(x, y);
 
-            var layerData :Int = layer.data[index];
-            var firstId :Int = tileset.firstgid;
-            var tileType :Int = layerData - firstId;
-            /*
-            switch (tileType) {
-                case 172: entity.add(new StraightTile(_ctx, x, y, rotation = 0));
-                case 170: entity.add(new StraightTile(_ctx, x, y, rotation = 1));
-                case 14: entity.add(new BendTile(_ctx, x, y, rotation = 0));
-                case 0: entity.add(new EmptyTile(_ctx, x, y, rotation));
-                case 2: entity.add(new GoalTile(_ctx, x, y, rotation = 2));
-                case 1: entity.add(new BlockTile(_ctx, x, y, 0));
-                default: trace("Unkown tile type: ", type);
+                    var layerData :Int = layer.data[index];
+                    var firstId :Int = tileset.firstgid;
+                    var tileType :Int = layerData - firstId;
+
+                    if (tileType >= 0) {
+                        var tileImageX :Int = Math.floor((tileType * tileset.tilewidth) % tileset.imagewidth * tileset.tilewidth);
+                        var tileImageY :Int = Math.floor((tileType * tileset.tileheight) / tileset.imageheight  * tileset.tileheight);
+                        var sprite = new ImageSprite(layerTexture.subTexture(tileImageX, tileImageY, tileset.tileheight, tileset.tilewidth));
+                        sprite.centerAnchor();
+                        sprite.setXY(tilemap.getViewWidth() / 2, tilemap.getViewHeight() / 2);
+                        sprite.x.animateTo(tilemap.tileToView(x), 1 + Math.random(), Ease.elasticOut);
+                        sprite.y.animateTo(tilemap.tileToView(y), 1 + Math.random(), Ease.elasticOut);
+                        sprite.scaleX.animateTo(1.0, 1 + Math.random(), Ease.elasticOut);
+                        sprite.scaleY.animateTo(1.0, 1 + Math.random(), Ease.elasticOut);
+                        entity.add(sprite);
+                    }
+                    
+                    // onTileCreated.emit(entity);
+                    // owner.addChild(entity);
+                }
+            } else if (layer.type == "objectgroup") {
+                for (objectIndex in 0...layer.objects.length) {
+                    var object :Dynamic = layer.objects[objectIndex];
+                    trace("Object: '" + object.name + "' at " + (object.x / tileset.tilewidth) + " x " + (object.y / tileset.tileheight));
+                }
             }
-
-            var tileSprite = entity.get(Sprite);
-            tileSprite.centerAnchor();
-            tileSprite.setXY(tilemap.getViewWidth() / 2, tilemap.getViewHeight() / 2);
-            tileSprite.x.animateTo(tilemap.tileToView(x), 1 + Math.random(), Ease.elasticOut);
-            tileSprite.y.animateTo(tilemap.tileToView(y), 1 + Math.random(), Ease.elasticOut);
-            tileSprite.scaleX.animateTo(1.0, 1 + Math.random(), Ease.elasticOut);
-            tileSprite.scaleY.animateTo(1.0, 1 + Math.random(), Ease.elasticOut);
-            */
-
-            if (tileType >= 0) {
-                var tileImageX :Int = Math.floor((tileType * tileset.tilewidth) % tileset.imagewidth * tileset.tilewidth);
-                var tileImageY :Int = Math.floor((tileType * tileset.tileheight) / tileset.imageheight  * tileset.tileheight);
-                var sprite = new ImageSprite(layerTexture.subTexture(tileImageX, tileImageY, tileset.tileheight, tileset.tilewidth));
-                sprite.centerAnchor();
-                sprite.setXY(tilemap.getViewWidth() / 2, tilemap.getViewHeight() / 2);
-                sprite.x.animateTo(tilemap.tileToView(x), 1 + Math.random(), Ease.elasticOut);
-                sprite.y.animateTo(tilemap.tileToView(y), 1 + Math.random(), Ease.elasticOut);
-                sprite.scaleX.animateTo(1.0, 1 + Math.random(), Ease.elasticOut);
-                sprite.scaleY.animateTo(1.0, 1 + Math.random(), Ease.elasticOut);
-                entity.add(sprite);
-            }
-            
-            // onTileCreated.emit(entity);
-            // owner.addChild(entity);
         }
+        
         return tilemap;
     }
 }
